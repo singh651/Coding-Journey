ab96b0e969b7250eeb03ac17d4179bbb562adc2f
Parikshit Singh <65848309+singh651@users.noreply.github.com>
2024-02-19 06:52:34 +0000

    Longest sub - array with sum k

---
+++ Striver_Arrays/Longest subarray with sum k (Contains zeroes, positives & negatives).cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a[1000],n;
+    cout << "Enter the number of elements : ";
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    int k;
+    cout << "Enter the required sub-array sum : ";
+    cin >> k;
+
+    map<int, int> prefixsum;
+    int sum = 0;
+    int maxlength = 0;
+    for(int i = 0; i < n; i++){
+        sum = sum + a[i];
+        if(sum == k){
+            maxlength = max(maxlength, i+1);
+        }
+
+        int rem = sum - k;
+        if(prefixsum.find(rem) != prefixsum.end()){
+            int len = i - prefixsum[rem];
+            maxlength = max(maxlength, len); 
+        }
+
+        prefixsum[sum] = i;
+
+    }
+
+    cout << "MaxLength : " << maxlength;
+
+}
