4df5d018798ba6fd4b48cea55814655a5e44573d
Parikshit Singh <65848309+singh651@users.noreply.github.com>
2024-03-21 11:31:27 +0000

    Bit manipulation

---
+++ Striver_Bit Manipulation/Remove the last set bit.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a;
+    cout << "Enter the number : ";
+    cin >> a;
+    int b;
+    b = a-1;
+    int x;
+    x = b & a;
+    cout << x;
+}
